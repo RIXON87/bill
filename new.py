f7d6cb9fcec03dcb5eed20e846f028403c189e03
RIXON87 <140376874+RIXON87@users.noreply.github.com>
2023-07-26 12:58:13 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,31 @@
+print("1. Simple Tea.\n2. Masala Tea\n3. Elaichi Tea\n4. Malai Tea\n5. Special Tea ")
+x=input("Enter your order according to number on display \n")
+if(x==1):
+    print("Your order is Simple Tea\n")
+elif(x==2):
+    print("Your order is Masala Tea\n")
+elif(x==3):
+    print("Your order is Elaichi Tea\n")
+elif(x==4):
+    print("Your order is Malaii Tea\n")
+elif(x==5):
+    print("Your order is Special Tea\n")
+
+i=1
+while i==1:
+    print("Do you want more items----->")
+    i = input("Enter 0 for no and 1 for yes : ")
+    if(i==0):
+        break
+
+    x=input("Enter your order according to number on display \n")
+    if(x==1):
+        print("Your order is Simple Tea\n")
+    elif(x==2):
+        print("Your order is Masala Tea\n")
+    elif(x==3):
+        print("Your order is Elaichi Tea\n")
+    elif(x==4):
+        print("Your order is Malaii Tea\n")
+    elif(x==5):
+        print("Your order is Special Tea\n")    
